179a29b1782e4ca68a889cd88951940473c66eec
maxwen <max.weninger@gmail.com>
2016-04-08 22:28:46 +0200

    [3/4] n3: refactor common NFC options
    Change-Id: Ia4f4b1bc20c910642d4e743855af2d3d9716e6c9

--- device.mk
+++ device.mk
@@ -34,12 +34,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     NfcNci \
     nfc_nci.n3 \
-    Tag \
     librmnetctl \
     OmniClick
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
